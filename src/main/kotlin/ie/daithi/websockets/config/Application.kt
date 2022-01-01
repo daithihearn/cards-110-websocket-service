a1e1e0c83f4c97682375709126e24f2150c9d51a
Daithi Hearn <dahearn@deloitte.ie>
2022-01-01 19:01:34 +0100

    BugFix Fixing error when building bootable jar

--- src/main/kotlin/ie/daithi/websockets/config/Application.kt
+++ src/main/kotlin/ie/daithi/websockets/config/Application.kt
@@ -4,12 +4,8 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class Application {
+class Application
 
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            SpringApplication.run(Application::class.java, *args)
-        }
-    }
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
 }
